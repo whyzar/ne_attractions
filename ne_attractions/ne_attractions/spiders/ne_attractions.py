13f07b4b6dc02beebc0b17091082f88b19ce7df0
whyzar <kasedr@gmail.com>
2019-12-19 22:42:30 -0500

    create parse method to parse h4 text of names and titles of attractions

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -4,3 +4,7 @@ class NorthEastAttractionsSpider(scrapy.Spider):
     name = 'ne_attractions'
     attractions_url = 'https://www.jurnii.com/rv_guide/us_ne_reg_attractions.php'
     start_url = [attractions_url]
+
+    def parse(self, response):
+        # Extract names/titles of attractions
+        h4tags = response.css("h4::text").extract()
