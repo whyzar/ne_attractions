0b5a178f4cb58fbf4778ce5e33210a2e24d28535
whyzar <kasedr@gmail.com>
2019-12-19 22:54:06 -0500

    extract and yield h4 text of names and titles of attractions

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -8,3 +8,10 @@ class NorthEastAttractionsSpider(scrapy.Spider):
     def parse(self, response):
         # Extract names/titles of attractions
         h4tags = response.css("h4::text").extract()
+
+        # Extracted data
+        for idx, bq in enumerate(response.css("blockquote")):
+            data = bq.css("p.smaller_par::text").extract()
+        yield { "title": h4text[idx] }
+
+
