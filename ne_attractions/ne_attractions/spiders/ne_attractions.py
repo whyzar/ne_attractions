39a4025c41bc3eec5a2fa166daaed9457cefa8e6
whyzar <kasedr@gmail.com>
2019-12-19 23:40:28 -0500

    updated function to capture only title and address <corrected line entry>

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -5,7 +5,7 @@ class NorthEastAttractionsSpider(scrapy.Spider):
     attractions_url = 'https://www.jurnii.com/rv_guide/us_ne_reg_attractions.php'
     start_url = [attractions_url]
 
-    def parse(self, response):
+    def parse(self,response):
         data = {}
         data['title'] = response.css('h4::text').extract()
         data['address'] = response.css('p.smaller_par::text').extract()
