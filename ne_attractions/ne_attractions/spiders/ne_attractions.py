8f31645bc10f3a3050647dece04782487e266ac1
whyzar <kasedr@gmail.com>
2019-12-19 22:35:23 -0500

    added class which includes url to scrape from

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -1,1 +1,6 @@
 import scrapy
+
+class NorthEastAttractionsSpider(scrapy.Spider):
+    name = 'ne_attractions'
+    attractions_url = 'https://www.jurnii.com/rv_guide/us_ne_reg_attractions.php'
+    start_url = [attractions_url]
