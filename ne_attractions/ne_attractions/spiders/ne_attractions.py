c2443b46c4540081daf764d75482017e966f2136
whyzar <kasedr@gmail.com>
2020-01-06 20:53:00 -0500

    modifyname of variable

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -3,7 +3,7 @@ import scrapy
 class NorthEastAttractionsSpider(scrapy.Spider):
     name = 'ne_attractions'
     attractions_url = 'https://www.jurnii.com/rv_guide/us_ne_reg_attractions.php'
-    start_url = [attractions_url]
+    start_urls = [attractions_url]
 
     def parse(self,response):
         data = {}
