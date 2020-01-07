c70cd6243baf4b5481c595c753e5d9231df634d9
whyzar <kasedr@gmail.com>
2020-01-06 21:15:19 -0500

    clean up of ascii and added comments for scraper function as well as data scrape block code

--- ne_attractions/ne_attractions/spiders/ne_attractions.py
+++ ne_attractions/ne_attractions/spiders/ne_attractions.py
@@ -1,14 +1,17 @@
 import scrapy
 
 class NorthEastAttractionsSpider(scrapy.Spider):
+    # scraper class for url
     name = 'ne_attractions'
     attractions_url = 'https://www.jurnii.com/rv_guide/us_ne_reg_attractions.php'
     start_urls = [attractions_url]
 
     def parse(self,response):
+        # Extract titles/names and data cleanup
         data = {}
         data['title'] = response.css('h4::text').extract()
         data['address'] = response.css('p.smaller_par::text').extract()
+        data[val.encode('ascii',ignore) for val in data]
         yield data
 
 
